006d94f0adc2f950997ea8e5eb43df1c1cdd8233
p-robot <will@probert.co.nz>
2021-07-05 16:50:18 +0100

    Add figure for generating plots of hospitalisations

--- run.sh
+++ run.sh
@@ -3,23 +3,21 @@
 # Script for generating figures and parameter templates for the CoMo model
 # for modelling analysis looking at public health and social measures against COVID-19 in Egypt
 
-
-
-
+#Rscript src/plot_como_output_calibration.R
 
 ####################################################
 # SENSITIVITY ANALYSIS TO MASK EFFICACY AND COVERAGE
 # --------------------------------------------------
 
 # Run sensitivity analysis across a range of attach rates
-for AR in 11 20 30
+for AR in 10 20 30
 do
 	# Create a directory for output
 	mkdir -p "data/input/AR${AR}"
 	
 	# Create a sensitivity analysis across mask efficacy and mask coverage
 	python3 src/egypt_mask_sensitivity.py \
-		"data/input/Egypt_V15_masksensitivity_AR${AR}.xlsx" \
+		"data/input/EGY_V17.2_AR${AR}.xlsx" \
 		"data/input/AR${AR}"
 	
 	# Compress the resultant folder
@@ -27,7 +25,18 @@ do
 done
 
 
+# Figure 1
+Rscript src/plot_attack_rates.R \
+    "data/output" \
+    "results/figures"
+
 # Figure 5
 Rscript src/plot_mask_eff_cov.R \
 	"data/output" \
 	"results/figures"
+
+# Figures of hospitalisations
+# (produces six figures)
+Rscript src/plot_hospitalisation.R \
+    "data/output/21.06.28" \
+    "results/figures"
